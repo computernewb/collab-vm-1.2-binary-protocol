eeadf3068851ebc80ad0df99e38d1870080fb7a1
wizelogic <211182326+wizelogic@users.noreply.github.com>
2025-05-31 11:13:38 -0400

    Add opus packet

--- src/CollabVMProtocolMessage.ts
+++ src/CollabVMProtocolMessage.ts
@@ -1,11 +1,15 @@
+// src/protocol/CollabVMProtocolMessage.ts (or similar)
 import { CollabVMRectMessage } from "./CollabVMRectMessage.js";
 
 export interface CollabVMProtocolMessage {
-    type: CollabVMProtocolMessageType;
-    rect?: CollabVMRectMessage | undefined;
+  type: CollabVMProtocolMessageType;
+  rect?: CollabVMRectMessage | undefined;
+  opusPacket?: Buffer | Uint8Array | undefined;
 }
 
 export enum CollabVMProtocolMessageType {
-    // JPEG Dirty Rectangle
-    rect = 0,
+  // JPEG Dirty Rectangle
+  rect = 0,
+  // Opus audio packet
+  audioOpus = 1
 }
